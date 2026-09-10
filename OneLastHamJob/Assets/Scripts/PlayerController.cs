dcce4b09b1e95f6685579944a04761a7253032e3
sonnycampbellhq <sonnycampbellhq@gmail.com>
2026-09-10 17:50:18 +0100

    alright, hopefully this should let my darlings lawrie and luca see this, if not idk. Added ray tracing from the player to the mouse, so we can see what they're shooting at

--- OneLastHamJob/Assets/Scripts/PlayerController.cs
+++ OneLastHamJob/Assets/Scripts/PlayerController.cs
@@ -36,8 +36,22 @@ public class PlayerController : MonoBehaviour
     {
         if (shoot.triggered)
         {
-            Debug.DrawLine(transform.position, camera.ScreenToWorldPoint(Mouse.current.position.ReadValue()), Color.black, 1);
-            Debug.Log("yuh");
+            Ray clickRayZPlane = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            float zPlaneVectorDistance = clickRayZPlane.origin.z/clickRayZPlane.direction.z;
+            Vector3 clickWorldSpaceVector = clickRayZPlane.origin - zPlaneVectorDistance*clickRayZPlane.direction;
+            Vector3 shootVector = clickWorldSpaceVector - transform.position;
+
+            RaycastHit[] hits = new RaycastHit[10];
+            Debug.Log(Physics.RaycastNonAlloc(transform.position, shootVector.normalized, hits));
+
+            Debug.Log(shootVector+":::"+hits[0].collider.gameObject.name);
+
+
+
+            Debug.DrawLine(transform.position, shootVector, Color.red, 5);
+
+
+
         }
     }
 
