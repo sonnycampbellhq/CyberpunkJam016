0469941452482891db82dbd2e55613c401a4365e
sonnycampbellhq <sonnycampbellhq@gmail.com>
2026-09-19 12:41:02 +0100

    checkpoint fix

--- OneLastHamJob/Assets/Scripts/PlayerController.cs
+++ OneLastHamJob/Assets/Scripts/PlayerController.cs
@@ -340,7 +340,7 @@ public class PlayerController : MonoBehaviour
     void OnTriggerEnter(Collider collider)
     {
         GameObject colliderGO = collider.gameObject;
-        if (colliderGO.tag != "Enemy")
+        if (colliderGO.tag != "Enemy"&&colliderGO.tag != "Checkpoint")
         {
             interactableItems.Add(collider.gameObject);
             collider.gameObject.GetComponentInChildren<BillboardController>().setInRange(true);
@@ -350,7 +350,7 @@ public class PlayerController : MonoBehaviour
     void OnTriggerExit(Collider collider)
     {
         GameObject colliderGO = collider.gameObject;
-        if (colliderGO.tag != "Enemy")
+        if (colliderGO.tag != "Enemy"&&colliderGO.tag != "Checkpoint")
         {
             interactableItems.Remove(collider.gameObject);
             collider.gameObject.GetComponentInChildren<BillboardController>().setInRange(false);
